fd50cb3af196d49ea0f38115d3d85e25ba4b523a
strtw <souporstu@gmail.com>
2017-03-03 11:51:28 -0800

    added scrollSpy function and file

---
+++ scripts/scrollSpy.js
@@ -0,0 +1,9 @@
+/**
+ * Created by stu on 3/3/17.
+ */
+window.onload = function(){
+    var body = document.getElementById("body");
+    body.addEventListener("scroll",function(){
+        console.log("Scrolling");
+    })
+}
