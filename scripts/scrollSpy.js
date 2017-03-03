31b319b7512f732e5e326bf6284957e673ab7581
strtw <souporstu@gmail.com>
2017-03-03 12:28:08 -0800

    added scrollSpy function and ids for some headers

--- scripts/scrollSpy.js
+++ scripts/scrollSpy.js
@@ -2,8 +2,8 @@
  * Created by stu on 3/3/17.
  */
 window.onload = function(){
-    var body = document.getElementById("body");
-    body.addEventListener("scroll",function(){
+    window.addEventListener("scroll",function(){
         console.log("Scrolling");
-    })
-}
+    });
+};
+
