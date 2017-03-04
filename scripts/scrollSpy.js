2caff0fc037d0a1bbf85f3822f9fd9679767329a
strtw <souporstu@gmail.com>
2017-03-03 17:19:30 -0800

    trigger scroll function on scroll

--- scripts/scrollSpy.js
+++ scripts/scrollSpy.js
@@ -1,9 +1,33 @@
 /**
  * Created by stu on 3/3/17.
  */
-window.onload = function(){
-    window.addEventListener("scroll",function(){
-        console.log("Scrolling");
-    });
-};
 
+
+
+
+(function(){
+
+    window.addEventListener("scroll",function(){
+        var dataItems = document.querySelectorAll(".componentSection__header");
+        var navItems = document.querySelectorAll(".sideBar__items a");
+        for(var i = 0; i < navItems.length;i++){
+            var navLinkHash =navItems[i].hash;
+            if(navLinkHash != ""){
+                for(var j = 0; j < dataItems.length; j++){
+                    var dataItemId = dataItems[j].getAttribute("id");
+                    var el = document.getElementById(dataItemId);
+                    if(navItems[i].hash == "#" + dataItemId){
+                        var screenPosition = dataItems[i].getBoundingClientRect();
+                        if(screenPosition.top > 200 && screenPosition.top < 250 ){
+                            navItems[i].style.color = "red";
+                        }
+                    }
+                }
+            }
+        }
+    });
+
+
+
+
+}());
