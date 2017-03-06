4fcb57f0bc3cb95582e28116c07044e71f2e9948
btmbtm <bmerri@williams-iMac.fios-router.home>
2017-03-05 21:11:55 -0500

    Add inline HTML display toggles

--- javascript/componentMarkupInjector.js
+++ javascript/componentMarkupInjector.js
@@ -3,10 +3,26 @@ var compHTML, compMarkupDisplay;
 var pre, code; 
 for(var i = 0; i < compContainers.length; i++){
 	compHTML = compContainers[i].querySelector('.componentContainer__display');
+
+
 	compMarkupDisplay = compContainers[i].querySelector('.componentContainer__markup');
 	if(compMarkupDisplay === null) continue;
 	pre = document.createElement('pre');
 	code = document.createElement('code');
+	markupToggleLabel = document.createElement('label');
+	
+	markupSwitch = document.createElement('input');
+	markupSwitch.type ="checkbox";
+	markupSwitch.id ="markupSwitch" + i;
+	markupSwitch.className = "componentSection_markupSwitch";
+
+	markupToggleLabel.setAttribute('for', markupSwitch.id);
+	markupToggleLabel.className ="button--primary";
+	markupToggleLabel.textContent = "Toggle HTML";
+
+	compMarkupDisplay.parentNode.insertBefore(markupToggleLabel, compMarkupDisplay);
+	compMarkupDisplay.parentNode.insertBefore(markupSwitch, compMarkupDisplay);
+
 
 	compCode = compHTML.innerHTML;
 	compCode = compCode.split('\n');
@@ -15,5 +31,4 @@ for(var i = 0; i < compContainers.length; i++){
 	code.innerText = compCode;
 	pre.appendChild(code);
 	compMarkupDisplay.appendChild(pre);
-	
 }
